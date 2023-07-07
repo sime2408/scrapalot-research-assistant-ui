66d1c7c5f1ca37b82d3e5160f78a813b386b2c87
sime2408 <simun.sunjic@gmail.com>
2023-07-07 16:43:10 +0200

    Added tooltips over each scratchpad button

--- src/components/ui/scratchpad/Scratchpad.js
+++ src/components/ui/scratchpad/Scratchpad.js
@@ -19,22 +19,22 @@ const Scratchpad = (props) => {
         }
 
         const buttons = [
-            {icon: 'arrow-counterclockwise', operation: () => editor.chain().focus().undo().run(), label: 'Undo'},
-            {icon: 'arrow-clockwise', operation: () => editor.chain().focus().redo().run(), label: 'Redo'},
-            {icon: 'type-bold', operation: () => editor.chain().focus().toggleBold().run(), label: 'Bold'},
-            {icon: 'type-italic', operation: () => editor.chain().focus().toggleItalic().run(), label: 'Italic'},
-            {icon: 'type-strikethrough', operation: () => editor.chain().focus().toggleStrike().run(), label: 'Strikethrough'},
-            {icon: 'paragraph', operation: () => editor.chain().focus().setParagraph().run(), label: 'Paragraph'},
-            {icon: 'type-h1', operation: () => editor.chain().focus().toggleHeading({ level: 1 }).run(), label: 'Heading 1'},
-            {icon: 'type-h2', operation: () => editor.chain().focus().toggleHeading({ level: 2 }).run(), label: 'Heading 2'},
-            {icon: 'type-h3', operation: () => editor.chain().focus().toggleHeading({ level: 3 }).run(), label: 'Heading 3'},
-            {icon: 'list-ul', operation: () => editor.chain().focus().toggleBulletList().run(), label: 'Bullet List'},
-            {icon: 'list-ol', operation: () => editor.chain().focus().toggleOrderedList().run(), label: 'Ordered List'},
-            {icon: 'code', operation: () => editor.chain().focus().toggleCode().run(), label: 'Code'},
-            {icon: 'braces', operation: () => editor.chain().focus().toggleCodeBlock().run(), label: 'Code Block'},
-            {icon: 'blockquote-left', operation: () => editor.chain().focus().toggleBlockquote().run(), label: 'Blockquote'},
-            {icon: 'hr', operation: () => editor.chain().focus().setHorizontalRule().run(), label: 'Horizontal Rule'},
-            {icon: 'file-break', operation: () => editor.chain().focus().setHardBreak().run(), label: 'Hard Break'},
+            {icon: 'arrow-counterclockwise', operation: () => () => editor.chain().focus().undo().run(), label: 'Undo'},
+            {icon: 'arrow-clockwise', operation: () => () => editor.chain().focus().redo().run(), label: 'Redo'},
+            {icon: 'type-bold', operation: () => () => editor.chain().focus().toggleBold().run(), label: 'Bold'},
+            {icon: 'type-italic', operation: () => () => editor.chain().focus().toggleItalic().run(), label: 'Italic'},
+            {icon: 'type-strikethrough', operation: () => () => editor.chain().focus().toggleStrike().run(), label: 'Strikethrough'},
+            {icon: 'paragraph', operation: () => () => editor.chain().focus().setParagraph().run(), label: 'Paragraph'},
+            {icon: 'type-h1', operation: () => () => editor.chain().focus().toggleHeading({ level: 1 }).run(), label: 'Heading 1'},
+            {icon: 'type-h2', operation: () => () => editor.chain().focus().toggleHeading({ level: 2 }).run(), label: 'Heading 2'},
+            {icon: 'type-h3', operation: () => () => editor.chain().focus().toggleHeading({ level: 3 }).run(), label: 'Heading 3'},
+            {icon: 'list-ul', operation: () => () => editor.chain().focus().toggleBulletList().run(), label: 'Bullet List'},
+            {icon: 'list-ol', operation: () => () => editor.chain().focus().toggleOrderedList().run(), label: 'Ordered List'},
+            {icon: 'code', operation: () => () => editor.chain().focus().toggleCode().run(), label: 'Code'},
+            {icon: 'braces', operation: () => () => editor.chain().focus().toggleCodeBlock().run(), label: 'Code Block'},
+            {icon: 'blockquote-left', operation: () => () => editor.chain().focus().toggleBlockquote().run(), label: 'Blockquote'},
+            {icon: 'hr', operation: () => () => editor.chain().focus().setHorizontalRule().run(), label: 'Horizontal Rule'},
+            {icon: 'file-break', operation: () => () => editor.chain().focus().setHardBreak().run(), label: 'Hard Break'},
         ];
 
         return (
@@ -51,10 +51,14 @@ const Scratchpad = (props) => {
                     >
                         <button
                             style={{border: 'none', background: 'none'}}
-                            onClick={button.operation}
+                            onMouseDown={(event) => {
+                                event.preventDefault(); // Prevent the default mousedown behavior
+                                button.operation()();
+                            }}
                         >
                             <i className={`bi bi-${button.icon}`}></i>
                         </button>
+
                     </OverlayTrigger>
                 ))}
             </div>
