58468d1a0c3fa994d04d169c5b2f5545c8e85ee5
sime2408 <simun.sunjic@gmail.com>
2023-07-07 16:36:44 +0200

    Added tooltips over each scratchpad button

--- src/components/ui/scratchpad/Scratchpad.js
+++ src/components/ui/scratchpad/Scratchpad.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 import styles from './Scratchpad.module.css';
 import {StarterKit} from '@tiptap/starter-kit';
 import {EditorContent, useEditor} from '@tiptap/react';
@@ -11,140 +11,66 @@ const Scratchpad = (props) => {
             StarterKit,
         ],
         content: '<p>Hello World!</p>',
-    })
+    });
 
     const MenuBar = ({ editor }) => {
         if (!editor) {
-            return null
+            return null;
         }
 
-        return (
-            <>
-                <div className={styles.scratchpadToolbar}>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().undo().run()}
-                            disabled={
-                                !editor.can()
-                                    .chain()
-                                    .focus()
-                                    .undo()
-                                    .run()
-                            }>
-                        <i className="bi bi-arrow-counterclockwise"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().redo().run()}
-                            disabled={
-                                !editor.can()
-                                    .chain()
-                                    .focus()
-                                    .redo()
-                                    .run()
-                            }>
-                        <i className="bi bi-arrow-clockwise"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleBold().run()}
-                            disabled={
-                                !editor.can()
-                                    .chain()
-                                    .focus()
-                                    .toggleBold()
-                                    .run()
-                            }>
-                        <i className="bi bi-type-bold"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleItalic().run()}
-                            disabled={
-                                !editor.can()
-                                    .chain()
-                                    .focus()
-                                    .toggleItalic()
-                                    .run()
-                            }>
-                        <i className="bi bi-type-italic"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleStrike().run()}
-                            disabled={
-                                !editor.can()
-                                    .chain()
-                                    .focus()
-                                    .toggleStrike()
-                                    .run()
-                            }>
-                        <i className="bi bi-type-strikethrough"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().setParagraph().run()}>
-                        <i className="bi bi-paragraph"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}>
-                        <i className="bi bi-type-h1"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}>
-                        <i className="bi bi-type-h2"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}>
-                        <i className="bi bi-type-h3"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleBulletList().run()}>
-                        <i className="bi bi-list-ul"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleOrderedList().run()}>
-                        <i className="bi bi-list-ol"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleCode().run()}
-                            disabled={
-                                !editor.can()
-                                    .chain()
-                                    .focus()
-                                    .toggleCode()
-                                    .run()
-                            }>
-                        <i className="bi bi-code"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleCodeBlock().run()}>
-                        <i className="bi bi-braces"></i>
-                    </button>
+        const buttons = [
+            {icon: 'arrow-counterclockwise', operation: () => editor.chain().focus().undo().run(), label: 'Undo'},
+            {icon: 'arrow-clockwise', operation: () => editor.chain().focus().redo().run(), label: 'Redo'},
+            {icon: 'type-bold', operation: () => editor.chain().focus().toggleBold().run(), label: 'Bold'},
+            {icon: 'type-italic', operation: () => editor.chain().focus().toggleItalic().run(), label: 'Italic'},
+            {icon: 'type-strikethrough', operation: () => editor.chain().focus().toggleStrike().run(), label: 'Strikethrough'},
+            {icon: 'paragraph', operation: () => editor.chain().focus().setParagraph().run(), label: 'Paragraph'},
+            {icon: 'type-h1', operation: () => editor.chain().focus().toggleHeading({ level: 1 }).run(), label: 'Heading 1'},
+            {icon: 'type-h2', operation: () => editor.chain().focus().toggleHeading({ level: 2 }).run(), label: 'Heading 2'},
+            {icon: 'type-h3', operation: () => editor.chain().focus().toggleHeading({ level: 3 }).run(), label: 'Heading 3'},
+            {icon: 'list-ul', operation: () => editor.chain().focus().toggleBulletList().run(), label: 'Bullet List'},
+            {icon: 'list-ol', operation: () => editor.chain().focus().toggleOrderedList().run(), label: 'Ordered List'},
+            {icon: 'code', operation: () => editor.chain().focus().toggleCode().run(), label: 'Code'},
+            {icon: 'braces', operation: () => editor.chain().focus().toggleCodeBlock().run(), label: 'Code Block'},
+            {icon: 'blockquote-left', operation: () => editor.chain().focus().toggleBlockquote().run(), label: 'Blockquote'},
+            {icon: 'hr', operation: () => editor.chain().focus().setHorizontalRule().run(), label: 'Horizontal Rule'},
+            {icon: 'file-break', operation: () => editor.chain().focus().setHardBreak().run(), label: 'Hard Break'},
+        ];
 
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().toggleBlockquote().run()}>
-                        <i className="bi bi-blockquote-left"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-                        <i className="bi bi-hr"></i>
-                    </button>
-                    <button style={{border: 'none', background: 'none'}}
-                            onClick={() => editor.chain().focus().setHardBreak().run()}>
-                        <i className="bi bi-file-break"></i>
-                    </button>
-                </div>
-            </>
-        )
-    }
+        return (
+            <div className={styles.scratchpadToolbar}>
+                {buttons.map((button, index) => (
+                    <OverlayTrigger
+                        key={index}
+                        placement='top'
+                        overlay={
+                            <Tooltip id={`tooltip-${index}`}>
+                                {button.label}
+                            </Tooltip>
+                        }
+                    >
+                        <button
+                            style={{border: 'none', background: 'none'}}
+                            onClick={button.operation}
+                        >
+                            <i className={`bi bi-${button.icon}`}></i>
+                        </button>
+                    </OverlayTrigger>
+                ))}
+            </div>
+        );
+    };
 
     return (
         <div style={{padding: '8px 8px 8px 0', height: 'calc(100vh - 134px)'}}>
             <MenuBar editor={editor}/>
             <div className={styles.scratchpad}>
                 <div className={styles.scratchpadTextEditor}>
-
                     <EditorContent editor={editor} />
                 </div>
             </div>
         </div>
     );
-
-}
+};
 
 export default Scratchpad;
